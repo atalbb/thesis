860cfc2bbff548f1a6f397183069198d36dd4b48
Atal <atal.bajracharya@gmail.com>
2018-10-14 18:10:29 -0400

    1st commit. led and 7 segment in web page

---
+++ PMOD_WIFI.cache/ip/2017.2/860416b60ef6bf89/design_1_atal_AES_LUT_41clks_1_0_stub.v
@@ -0,0 +1,50 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
+// Date        : Wed Oct 10 14:26:01 2018
+// Host        : DESKTOP-GJPCRJL running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ design_1_atal_AES_LUT_41clks_1_0_stub.v
+// Design      : design_1_atal_AES_LUT_41clks_1_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "atal_AES_LUT_41clks_v2_0,Vivado 2017.2" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(keyIn, dataIn, data32, keyAddrOut, dataAddrOut, 
+  weOut, s_axi_awaddr, s_axi_awprot, s_axi_awvalid, s_axi_awready, s_axi_wdata, s_axi_wstrb, 
+  s_axi_wvalid, s_axi_wready, s_axi_bresp, s_axi_bvalid, s_axi_bready, s_axi_araddr, 
+  s_axi_arprot, s_axi_arvalid, s_axi_arready, s_axi_rdata, s_axi_rresp, s_axi_rvalid, 
+  s_axi_rready, s_axi_aclk, s_axi_aresetn)
+/* synthesis syn_black_box black_box_pad_pin="keyIn[31:0],dataIn[31:0],data32[31:0],keyAddrOut[31:0],dataAddrOut[31:0],weOut[3:0],s_axi_awaddr[3:0],s_axi_awprot[2:0],s_axi_awvalid,s_axi_awready,s_axi_wdata[31:0],s_axi_wstrb[3:0],s_axi_wvalid,s_axi_wready,s_axi_bresp[1:0],s_axi_bvalid,s_axi_bready,s_axi_araddr[3:0],s_axi_arprot[2:0],s_axi_arvalid,s_axi_arready,s_axi_rdata[31:0],s_axi_rresp[1:0],s_axi_rvalid,s_axi_rready,s_axi_aclk,s_axi_aresetn" */;
+  input [31:0]keyIn;
+  input [31:0]dataIn;
+  output [31:0]data32;
+  output [31:0]keyAddrOut;
+  output [31:0]dataAddrOut;
+  output [3:0]weOut;
+  input [3:0]s_axi_awaddr;
+  input [2:0]s_axi_awprot;
+  input s_axi_awvalid;
+  output s_axi_awready;
+  input [31:0]s_axi_wdata;
+  input [3:0]s_axi_wstrb;
+  input s_axi_wvalid;
+  output s_axi_wready;
+  output [1:0]s_axi_bresp;
+  output s_axi_bvalid;
+  input s_axi_bready;
+  input [3:0]s_axi_araddr;
+  input [2:0]s_axi_arprot;
+  input s_axi_arvalid;
+  output s_axi_arready;
+  output [31:0]s_axi_rdata;
+  output [1:0]s_axi_rresp;
+  output s_axi_rvalid;
+  input s_axi_rready;
+  input s_axi_aclk;
+  input s_axi_aresetn;
+endmodule
